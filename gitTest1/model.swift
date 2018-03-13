5c5b3016630ef444ff5a2e28348ca85540739416
Davide Compagnone <davidecompagnone@cam-02.local>
2018-03-13 11:57:18 +0100

    aggiunta della classe model

---
+++ gitTest1/model.swift
@@ -0,0 +1,11 @@
+//
+//  model.swift
+//  gitTest1
+//
+//  Created by Davide Compagnone on 13/03/18.
+//  Copyright © 2018 Davide Compagnone. All rights reserved.
+//
+
+import Foundation
+
+//file Added
